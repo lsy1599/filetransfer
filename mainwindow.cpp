14cebbd724795f5aeb9958daf779902d2020b9b5
卢孙远 <476941913@qq.com>
2017-04-05 21:22:30 +0800

    布局好了页面

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,12 @@ MainWindow::~MainWindow()
 {
   delete ui;
 }
+
+void MainWindow::on_pushButton_exit_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setWindowTitle("关闭程序");
+    msgBox.setText("注意,程序即将关闭.");
+    msgBox.exec();
+    this->close();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 
 namespace Ui {
   class MainWindow;
@@ -14,6 +15,10 @@ class MainWindow : public QMainWindow
 public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
+
+private slots:
+
+  void on_pushButton_exit_clicked();
 
 private:
   Ui::MainWindow *ui;
